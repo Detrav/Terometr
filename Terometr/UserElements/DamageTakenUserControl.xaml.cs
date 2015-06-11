d5c2be025b6a63bf510adf83fadc3328a9714b62
Detrav <witalyezep@gmail.com>
2015-06-11 16:37:05 +0300

    Добавил функции поиска бам и обновления комбобокса

--- Terometr/UserElements/DamageTakenUserControl.xaml.cs
+++ Terometr/UserElements/DamageTakenUserControl.xaml.cs
@@ -113,5 +113,56 @@ namespace Detrav.Terometr.UserElements
         {
             throw new NotImplementedException();
         }
+
+        public void comboBoxReMake()
+        {
+            //если пусто то добавляем всего строку
+            if (comboBox.Items.Count == 0)
+                comboBox.Items.Add(new ComboBoxHiddenItem(UInt64.MaxValue, "Суммарно"));
+            var selectDb = db;
+            if (toggleButtonGroup.IsChecked == true) selectDb = dbGrp;
+            while (comboBox.Items.Count > selectDb.Count + 1) comboBox.Items.RemoveAt(0);
+            while (comboBox.Items.Count < selectDb.Count + 1) comboBox.Items.Insert(comboBox.Items.Count - 1, new ComboBoxHiddenItem(0, null));
+
+            int i = 0;
+            foreach (var key in selectDb.Keys.ToArray())
+            {
+                var comboBoxItem = comboBox.Items[i] as ComboBoxHiddenItem;
+                if (comboBoxItem != null)
+                {
+                    if (key != comboBoxItem.id)
+                    {
+                        comboBoxItem.id = key;
+                        comboBoxItem.text = selectDb[key].name;
+                    }
+                }
+                i++;
+            }
+            if (comboBox.SelectedItem == null) comboBox.SelectedIndex = comboBox.Items.Count - 1;
+        }
+
+        public void selectBam()
+        {
+            var selectDb = db;
+            if (toggleButtonGroup.IsChecked == true) selectDb = dbGrp;
+            if (toggleButtonBAM.IsChecked == true)
+            {
+                int i = 0;
+                int max_i = -1;
+                uint max = 0;
+                foreach (var pair in selectDb)
+                {
+                    if (pair.Value.isActive)
+                        if (pair.Value.npcHp > max)
+                        {
+                            max = pair.Value.npcHp;
+                            max_i = i;
+                        }
+                    i++;
+                }
+                if (max_i < 0) max_i = i;
+                comboBox.SelectedIndex = max_i;
+            }
+        }
     }
 }
